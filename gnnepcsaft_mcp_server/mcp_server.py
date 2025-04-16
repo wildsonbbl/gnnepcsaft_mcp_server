149c860c08971f1aafb118cff922f1940ff5d550
wildsonbbl <wil_bbl@hotmail.com>
2025-04-16 16:59:38 -0300

    add critical points

--- gnnepcsaft_mcp_server/mcp_server.py
+++ gnnepcsaft_mcp_server/mcp_server.py
@@ -4,6 +4,7 @@ from typing import Any, Callable, List
 
 from gnnepcsaft.data.rdkit_util import inchitosmiles, mw, smilestoinchi
 from gnnepcsaft.epcsaft.epcsaft_feos import (
+    critical_points_feos,
     mix_den_feos,
     mix_vp_feos,
     pure_den_feos,
@@ -28,6 +29,7 @@ fn_list: List[Callable[..., Any]] = [
     prediction,
     inchitosmiles,
     pure_h_lv_feos,
+    critical_points_feos,
 ]
 for fn in fn_list:
     mcp.add_tool(fn)
