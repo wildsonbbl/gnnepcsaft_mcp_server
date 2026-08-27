3353965748be736da8417edd836573e10bacc054
wildsonbbl <wil_bbl@hotmail.com>
2026-08-27 20:58:28 -0300

    change tools order

--- gnnepcsaft_mcp_server/mcp_server.py
+++ gnnepcsaft_mcp_server/mcp_server.py
@@ -21,14 +21,14 @@ mcp = MCPServer("gnnpcsaft")
 fn_list: List[Callable[..., Any]] = [
     batch_predict_pcsaft_parameters,
     batch_critical_points,
-    batch_inchi_to_smiles,
-    batch_smiles_to_inchi,
-    batch_molecular_weights,
     batch_pure_density,
     batch_pure_h_lv,
     batch_pure_vapor_pressure,
     mixture_density,
     mixture_vapor_pressure,
+    batch_inchi_to_smiles,
+    batch_smiles_to_inchi,
+    batch_molecular_weights,
 ]
 for fn in fn_list:
     mcp.add_tool(fn)
